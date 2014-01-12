d7232b75939ae8eb64b6a75c0841258be7ed9dff
sgarcia <sgarcialaguna@googlemail.com>
2014-01-12 16:34:30 +0100

    problem 6

---
+++ problem6.py
@@ -0,0 +1,10 @@
+sumOfSquares = 0
+squaredSums = 0
+
+for i in range(1,101):
+    sumOfSquares += i**2
+    squaredSums += i
+
+squaredSums **= 2
+
+print sumOfSquares, squaredSums, sumOfSquares - squaredSums
