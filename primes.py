3465a7dd61f4e4916d8f5878c0c63229c68f1fe1
santa <sgarcialaguna@googlemail.com>
2015-12-04 22:31:48 +0100

    Some cleanup, port to Python 3.

--- primes.py
+++ primes.py
@@ -1,3 +1,6 @@
+import math
+
+
 def prime_factorization(n):
     if n < 1:
         raise NotImplemented
@@ -18,6 +21,15 @@ def prime_factorization(n):
 
 def is_prime(n):
     return len(prime_factorization(n)) == 1
+
+
+def prime_sieve(n):
+    is_prime = [False, False] + [True] * (n - 1)
+    for i in range(2, int(math.sqrt(n) + 1)):
+        if is_prime[i]:
+            for j in range(i * i, n + 1, i):
+                is_prime[j] = False
+    return [idx for idx, i in enumerate(is_prime) if i]
 
 
 assert (prime_factorization(1) == [1])
--- problem10.py
+++ problem10.py
@@ -1,12 +1,3 @@
-import math
+import primes
 
-
-def prime_sieve(n):
-    is_prime = [False, False] + [True] * (n-1)
-    for i in range(2, int(math.sqrt(n) + 1)):
-        if is_prime[i]:
-            for j in range(i*i, n+1, i):
-                is_prime[j] = False
-    return [idx for idx, i in enumerate(is_prime) if i]
-
-print sum(prime_sieve(2000000))
+print(sum(primes.prime_sieve(2000000)))
