42266f1efa51a15f2988f0a88e3d20ae70d5ec22
santa <sgarcialaguna@googlemail.com>
2015-12-04 20:58:57 +0100

    Some cleanup, port to Python 3

--- problem1.py
+++ problem1.py
@@ -1,7 +1,11 @@
-if __name__ == '__main__':
-    sum = 0
-    for i in range(3,1000):
-        if i % 3 == 0 or i % 5 == 0:
-            sum += i
+"""If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000."""
 
-    print sum
+if __name__ == '__main__':
+    sum_ = 0
+    for i in range(3, 1000):
+        if i % 3 == 0 or i % 5 == 0:
+            sum_ += i
+
+    print(sum_)
