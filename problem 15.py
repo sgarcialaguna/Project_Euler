b446fc7164553c74df8116931c0df19dc2b6956d
santa <sgarcialaguna@googlemail.com>
2015-12-05 13:31:41 +0100

    Some cleanup, port to Python 3.

--- problem 15.py
+++
@@ -1,2 +0,0 @@
-import math
-print math.factorial(40) / (math.factorial(20) * math.factorial(20))
--- problem14.py
+++ problem14.py
@@ -1,3 +1,17 @@
+"""The following iterative sequence is defined for the set of positive integers:
+
+n → n/2 (n is even)
+n → 3n + 1 (n is odd)
+
+Using the rule above and starting with 13, we generate the following sequence:
+
+13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+
+Which starting number, under one million, produces the longest chain?
+
+NOTE: Once the chain starts the terms are allowed to go above one million."""
+
 def collatz_sequence(n):
     sequence = [n]
     while sequence[-1] != 1:
@@ -27,9 +41,7 @@ def find_longest_collatz_sequence():
         c = collatz_sequence(i)
         if c > longest_collatz_sequence[1]:
             longest_collatz_sequence = (i, c)
-    assert(longest_collatz_sequence == (837799, 525))
-    print longest_collatz_sequence
+    print(longest_collatz_sequence)
 
 
-import timeit
-print timeit.timeit(find_longest_collatz_sequence, number=1)
+find_longest_collatz_sequence()
