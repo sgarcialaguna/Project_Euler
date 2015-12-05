3215431e60e26e772eb6a8deb9acc8ee71ddb90d
santa <sgarcialaguna@googlemail.com>
2015-12-05 13:32:34 +0100

    Some cleanup, port to Python 3.

--- problem16.py
+++ problem16.py
@@ -1,4 +1,8 @@
+"""215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 21000?"""
+
 sum = 0
 for n in str(2**1000):
     sum += int(n)
-print sum
+print(sum)
