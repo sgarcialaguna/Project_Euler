d5c773531d92f0d0c775091bfa948e27e90594cb
santa <sgarcialaguna@googlemail.com>
2015-12-05 13:34:16 +0100

    Some cleanup, port to Python 3.

--- problem17.py
+++ problem17.py
@@ -1,16 +1,27 @@
+"""If the numbers 1 to 5 are written out in words: one, two, three, four, five,
+then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+
+If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words,
+how many letters would be used?
+
+
+NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains
+23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing
+out numbers is in compliance with British usage."""
+
 def numberToWord(n):
     word = ''
     if n == 1000:
         return 'one thousand'
     if n >= 100:
-        word += numberToWord.lookup[n / 100] + ' hundred'
+        word += numberToWord.lookup[n // 100] + ' hundred'
         remainder = numberToWord(n % 100)
         if remainder:
             word += ' and ' + remainder
         return word
     if n / 10 < 2:
         return numberToWord.lookup[n]
-    word = numberToWord.lookup[n / 10 * 10]
+    word = numberToWord.lookup[n // 10 * 10]
     if n % 10 != 0:
         word += '-' + numberToWord.lookup[n % 10]
     return word
@@ -78,4 +89,4 @@ assert (numberToWord(1000) == 'one thousand')
 total_length = 0
 for i in range(1, 1001):
     total_length += len(numberToWord(i).replace(' ', '').replace('-', ''))
-print total_length
+print(total_length)
